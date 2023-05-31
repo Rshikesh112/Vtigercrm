c265645a6c42323ffd1c921018ea91cfa0c9199a
Rshikesh112 <126946192+Rshikesh112@users.noreply.github.com>
2023-05-31 09:56:52 +0530

    Add files via upload

---
+++ classcanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class example
+{
+  public static void main(String[]arg)
+  System.out.print("Enter the First Number :");
+
+  int a =sc.nextInt();
+  System.out.print("Enter the second number :");
+
+  int b = sc.nextInt();
+  int c= a+b;
+
+  System.out.println("Addition of user entered 2 number is")
+}
+} 
